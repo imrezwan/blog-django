fff8a89277cebab21285e079eef4d877591084e0
rezwan2525 <rezwan2525@gmail.com>
2022-08-30 15:16:43 +0600

    added db

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    slug = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title[:50]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from pages.models import Post
 
 # def homePageView(request):
 #     return HttpResponse('Hello World')
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
 
 class AboutPageView(TemplateView):
