646d596fe8c090091cede6b5a2183e6c83baae08
rezwan2525 <rezwan2525@gmail.com>
2022-09-01 12:55:22 +0600

    test added

--- pages/tests.py
+++ pages/tests.py
@@ -1,9 +1,26 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
+from .models import Post
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(id= 1, title='just a test')
+    
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.title}'
+        self.assertEqual(expected_object_name, 'just a test')
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(title='this is another test')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
 
 class SimpleTests(SimpleTestCase):
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
     
     def test_about_us_status_code(self):
         response = self.client.get('/aboutus/')
