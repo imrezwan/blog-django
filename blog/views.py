a37628c6c18b197aaa76a315757998bc1d0a31a7
rezwan2525 <rezwan2525@gmail.com>
2022-09-01 15:09:36 +0600

    CRUD functionality

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from blog.models import BlogPost
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = BlogPost
@@ -10,3 +9,19 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = BlogPost
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = BlogPost
+    template_name = 'add_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = BlogPost
+    template_name = 'post_edit.html'
+    fields = '__all__'
+    # fields = [ 'title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
